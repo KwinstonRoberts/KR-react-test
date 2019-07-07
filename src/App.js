356e7cf183d6a874f78ce871c2c68c0f3f851626
KwinstonRoberts <kyle.w.r@hotmail.com>
2019-07-07 19:30:23 -0400

    qol changes

--- src/App.js
+++ src/App.js
@@ -13,11 +13,12 @@ class App extends Component {
   }
 
   fetchRestaurants(event){
-    const url = 'https://opentable.herokuapp.com/api/restaurants?city=' + event.target.value.toLowerCase();
+    const url = 'https://opentable.herokuapp.com/api/restaurants?city=' + event.target.value.toLowerCase() + '&state=ON';
     fetch(url)
     .then(response => response.json())
     .then(data => {
       if(data.restaurants){
+        console.log(data.restaurants)
         this.setState({restaurants: data.restaurants})
       } 
     });
@@ -26,7 +27,7 @@ class App extends Component {
   componentDidMount() {
     window.addEventListener('search', this.fetchRestaurants);
     // Call our fetch function below once the component mounts
-    fetch(`https://opentable.herokuapp.com/api/restaurants?city=toronto`)
+    fetch(`https://opentable.herokuapp.com/api/restaurants?city=toronto&state=ON`)
     .then(response => response.json())
     .then(data => this.setState({restaurants: data.restaurants})); 
   }
@@ -46,25 +47,29 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header has-background-primary">
           <h1 className="title has-text-white">Welcome to Kelp!<br/> The search engine for restaurants!</h1>
           <br/>
+          <h3 className="text-center">Enter a City to start looking!</h3>
          <SearchBar fetchRestaurants={this.fetchRestaurants}></SearchBar>  
         </header>
         <section className="section">
           <div className="columns is-multiline">
             { 
               this.state.restaurants.map(restaurant =>
-              <div className="column is-half has-text-white" key={restaurant.id}>
+              <div className="column is-one-third has-text-white" key={restaurant.id}>
                 <div className="card">
-                  <div className="card-header">
-                    <h1 className="card-header-title">{restaurant.name}</h1>
+                    <div className="card-header">
+                      <h1 className="card-header-title">{restaurant.name}</h1>
+                    </div>
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img src={restaurant.image_url}/>
+                    </figure>
                   </div>
                   <div className="card-content">
-                    <p>{restaurant.address}</p><br/>
-                    <div className="columns">
-                      <h1>{getPrice(restaurant.price)}</h1>
-                    </div>
+                    <p>{restaurant.address + ', ' + restaurant.postal_code}</p><br/>
+                    <h1>{getPrice(restaurant.price)}</h1>
                   </div>
                 </div>
               </div>
